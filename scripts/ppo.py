6bee5eacde88346ac4fbcf73afe02afb9b3b9b90
Tommy <t.kiss@campus.lmu.de>
2025-12-10 13:30:29 +0100

    refactor: filesystem

--- scripts/ppo.py
+++ scripts/ppo.py
@@ -1,7 +1,9 @@
 from stable_baselines3 import PPO
 import gymnasium as gym
 
+Model_PATH = "models/ppo_invertedpendulum.zip"
+
 env = gym.make("InvertedPendulum-v4")
 model = PPO("MlpPolicy", env, verbose=1)
 model.learn(total_timesteps=100_000)
-model.save("ppo_invertedpendulum")
+model.save(Model_PATH)
